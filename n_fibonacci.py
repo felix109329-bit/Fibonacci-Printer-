99934020d166808146b0a3f3af7ab925ed236757
felix109329-bit <felix109329@mcpsva.org>
2026-03-10 14:53:26 +0000

    m

--- n_fibonacci.py
+++ n_fibonacci.py
@@ -1,14 +1,26 @@
 """
 Filename: n_fibonacci.py
-Author: <NAME>
-Created: <DATE>
+Author: <Felix Olguin>
+Created: <2/1/26>
 Instructor: Holtslander
 """
 
 def n_fibonacci():
     # Write your code here
-
-
+    n = int(input("Please input a non-negative whole number.\n"))
+    for i in range (n):
+        if i == 0:
+            print(0)
+        elif i == 1:
+            print(1)
+        else:
+            a = 0
+            b = 1
+            for i in range (2, i + 1):
+                c = a + b
+                a = b
+                b = c
+            print(c)              
 # You should not need to change any code below this point
 def main():
     print("This program displays the standard Fibonacci sequence that is n elements long.")
